4443a6f7f32ee045a052caf75ae87d5f60b9c5d0
Mike Sheldrake <boards@sheldrake.net>
2013-07-05 23:48:02 -0700

    document (unimplemented) zFill constants and options

--- lib/Math/Clipper/WithZ.pm
+++ lib/Math/Clipper/WithZ.pm
@@ -24,6 +24,7 @@ our %EXPORT_TAGS = (
     polyfilltypes => [qw/PFT_EVENODD PFT_NONZERO PFT_POSITIVE PFT_NEGATIVE/],
     jointypes     => [qw/JT_MITER JT_ROUND JT_SQUARE/],
     endtypes     => [qw/ET_SQUARE ET_ROUND ET_BUTT ET_CLOSED/],
+    zfilltypes     => [qw/ZFT_NONE ZFT_MAX ZFT_MIN ZFT_MEAN ZFT_BOTH_UINT32/],
     utilities       => [qw/area offset is_counter_clockwise orientation integerize_coordinate_sets unscale_coordinate_sets
                     simplify_polygon simplify_polygons int_offset ex_int_offset ex_int_offset2/],
 );
@@ -228,6 +229,18 @@ Other than those additional features, this module should
 work identically to Math::Clipper. See the
 Math::Clipper docs for overall usage.
 
+=head1 EXPORTS
+
+To specify which predetermined zFill callback to use, this
+module defines a few additional constants that may be
+imported.
+
+    ZFT_NONE
+    ZFT_MAX
+    ZFT_MIN
+    ZFT_MEAN
+    ZFT_BOTH_UINT32
+
 =head1 METHODS
 
 All methods are the same as Math::Clipper except that
@@ -252,9 +265,39 @@ the point at the beginning of that edge.)
 =head1 Z FILL OPTIONS
 
 =head2 ZFT_BOTH_UINT32
+
+The Z values of the two edges that intersect will be interpreted as
+32 bit unsigned integers, and both will be returned.
+
+This could typically be used to identify the input edges that contribute 
+to the output intersection point, if the integers are indices into the 
+input edge/point list.
+
+Or it could be used to identify which input polygons contribute to an output
+polygon, and what subsequences of output polygon points come from which
+input polygons. In this case, Z values for all points in each polygon would 
+bear the same integer identifying the polygon.
+
 =head2 ZFT_MAX
+
+The intersection point will inherit the maximum Z value of the two 
+intersecting edges.
+
 =head2 ZFT_MIN
+
+The intersection point will inherit the minimum Z value of the two 
+intersecting edges.
+
 =head2 ZFT_MEAN
+
+The intersection point will inherit the arithmetic mean ((Z1+Z2)/2) of the 
+Z values of the two intersecting edges. 
+
+=head2 ZFT_NONE
+
+No callback function will be defined. Clipper will simply set Z to zero for
+all intersection points (while still passing through any Z values present in
+other points).
 
 =head1 NO Z FILL FOR OFFSETS
 
